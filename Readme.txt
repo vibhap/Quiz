7815f677fffc2c065382a7958032460ecf41feb3
Vibha Prahlada <vibha.prahlada@gmail.com>
2014-09-14 16:41:16 -0700

    Quiz files

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Name: Vibha Prahlada
+Course: COEN 276
+References: w3Schools, stackOverflow.com
---
+++ questions.js
@@ -0,0 +1,69 @@
+/*Name: Vibha Prahlada 
+  Course Name: Web Programming 1
+  Assignment Number: 2
+  Date of Submission: 10-29-2012 */
+var geoQuestions =[{question:  "Where is the great Barrier Reef?", 
+					questionType:  1, 
+					choices: [ "Africa", "N.America","S.America","Australia"],
+					correctChoice: "Australia",
+					score: 5,
+					hint:"It has one of the best beaches in the World."
+					},
+					{question:  "What planet is Curiosity Rover on now?", 
+					questionType:  2, 
+					correctChoice: "MARS",
+					score: 5,
+					hint: " This is the 4th planet from the sun and next planet to EARTH"
+					},
+					{question: "Which Among these is the largest country in Middle East in area?",
+					questionType: 1,
+					choices: ["Egypt","Saudi Arabia","Iran","Iraq"],
+					correctChoice:"Saudi Arabia",
+					score: 5,
+					hint:" This country has the world's second largest oil reserve."
+					},
+					{question:"What is the term for a part of an ocean or sea that cuts far into the bordering landmass and may contain one or more bays?",
+					questionType: 2,
+					correctChoice:"GULF",
+					score: 5,
+					hint:"The GULF of MEXICO is one of them."
+					},
+					{question:"Which state has a climate suitable for growing citrus fruits",
+					questionType:1,
+					choices:["California","Maine","New York","Alaska"],
+					correctChoice:"California",
+					score:5,
+					hint:"Bay Area!! Does it ring a bell!!"
+					},
+					{question:"The great Victoria Desert is located in",
+					questionType:1,
+					choices:["Canada", "West Africa", "Australia", "North America"],
+					correctChoice:"Australia",
+					score:5,
+					hint:"It is the native of Kangaroos and Koalas."
+					},
+					{question:"The leading state in India in producing paper is",
+					questionType:2,
+					correctChoice:"WEST BENGAL",
+					score:5,
+					hint:"This state in INDIA is famous for Tigers and Rabindranath Tagore is from this region!"
+					},
+					{question:"The largest gold producing country in the world is",
+					questionType:2,
+					correctChoice:"CHINA",
+					score:5,
+					hint:"2008 olympics was held in this country."
+					},
+					{question:"The largest country of the world by geographical area is",
+					questionType:1,
+					choices:["Russia","Vatican City", "Australia", "USA"],
+					correctChoice:"Russia",
+					score:5,
+					hint:"Moscow is the capital of this counrty."
+					},
+					{question:"The largest Continent in the world is",
+					questionType:2,
+					correctChoice:"ASIA",
+					score:5,
+					hint:"This continent hosts 60% of the world's current human population."
+					}];
